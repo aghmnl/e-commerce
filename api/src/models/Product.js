4c746d3e7027d0a6ce011cce6730c888a41ce613
aghmnl <agustin.minetto@gmail.com>
2020-08-13 11:36:35 -0300

    Agregado de active en Product, elimino logo, correción FormProduct

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,34 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('product', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false
-    },
-    description:{
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    img: {
-      type: DataTypes.BLOB,
-      allowNull: false
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-  });
+module.exports = sequelize => {
+	// defino el modelo
+	sequelize.define("product", {
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.DECIMAL,
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		img: {
+			type: DataTypes.BLOB,
+			allowNull: false,
+		},
+		stock: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		active: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			default: true,
+		},
+	});
 };
--- client/src/components/FormProduct.jsx
+++ client/src/components/FormProduct.jsx
@@ -1,39 +1,75 @@
-import React from "react";
-import React, { useState } from "react";
-export default function FormProduct(props) {
+import React, { useState, useEffect } from "react";
+import { matchPath } from "react-router-dom";
+import "../styles/FormProduct.css";
+export default function FormProduct(props = null) {
 	const [inputs, setInputs] = useState({
 		name: "",
 		description: "",
 		price: "",
 		stock: "",
 		img: "",
-		category: "",
-		cellar: "",
+		categoryId: "",
+		cellarId: "",
+		strainId: "",
+		active: true,
+		nombreBoton: "Agregar",
 	});
+	//var nombreBoton = "Agregar";
+	useEffect(() => {
+		if (Object.values(props).length > 0) {
+			setInputs({
+				name: props.name,
+				description: props.description,
+				price: props.price,
+				stock: props.stock,
+				img: props.img,
+				categoryId: props.categoryId,
+				cellarId: props.cellarId,
+				strainId: props.strainId,
+				active: props.active,
+				nombreBoton: "Actualizar",
+			});
+		}
+	}, []);
+
 	function handleSubmit(e) {
 		e.preventDefault();
 		//fetch a la api :
 	}
 	return (
-		<form onSubmit={handleSubmit(e)}>
-			<input type="text" key="name" placeholder="Nombre" onChange={e => setInputs({ ...inputs, name: e.target.value })} />
-			<input type="number" step="0.1" key="price" placeholder="Precio" onChange={e => setInputs({ ...inputs, price: e.target.value })} />
-			<textarea key="description" placeholder="Descripción" onChange={e => setInputs({ ...inputs, description: e.target.value })}></textarea>
-			<input id="archivo" accept="image/x-png,image/gif,image/jpeg" type="file" key="img" onChange={e => setInputs({ ...inputs, img: e.target.value })} />
+		<form id="formulario" onSubmit={e => handleSubmit(e)}>
+			<label for="inputName">Nombre de producto: </label>
+			<input id="inputName" value={props.name} type="text" key="name" placeholder="Nombre" onChange={e => setInputs({ ...inputs, name: e.target.value })} />
+			<label for="inputPrice">Precio de producto: </label>
+			<input id="inputPrice" type="number" step="0.01" key="price" placeholder="Precio" onChange={e => setInputs({ ...inputs, price: e.target.value })} />
+			<label for="inputDescription">Descripción del producto: </label>
+			<textarea id="inputDescription" key="description" placeholder="Descripción" onChange={e => setInputs({ ...inputs, description: e.target.value })}></textarea>
 			<label for="archivo">Imagen</label>
+			<input id="archivo" accept="image/x-png,image/gif,image/jpeg" type="file" key="img" onChange={e => setInputs({ ...inputs, img: e.target.value })} />
+			<label for="number">Stock del producto: </label>
 			<input type="number" key="stock" placeholder="Cantidad" onChange={e => setInputs({ ...inputs, stock: e.target.value })} />
+			<label for="category">Categorías: </label>
 			<select id="category" onChange={e => setInputs({ ...inputs, category: e.target.value })}>
 				<option value="Cat1">Categoría 1</option>
 				<option value="Cat2">Categoría 2</option>
 				<option value="Cat3">Categoría 3</option>
 			</select>
+			<label for="cellar">Bodega: </label>
 			<select id="cellar">
 				<option value="cell1">Bodega 1</option>
 				<option value="cell2">Bodega 2</option>
 				<option value="cell3">Bodega 3</option>
 			</select>
-
-			<button>Agregar</button>
+			<label for="strain">Bodega: </label>
+			<select id="strain">
+				<option value="strain1">Cepa 1</option>
+				<option value="strain2">Cepa 2</option>
+				<option value="strain3">Cepa 3</option>
+			</select>
+			<label>Estado de producto de tienda</label>
+			<input id="checkbox" type="checkbox" key="active" value="active_checkbox" onChange={e => setInputs({ ...inputs, active: e.target.value })} />
+			<p></p>
+			<button>{inputs.nombreBoton}</button>
 		</form>
 	);
 }
