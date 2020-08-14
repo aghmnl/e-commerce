ea9cbbdadb107c6f46511a2fa03a18db7c613c52
aghmnl <agustin.minetto@gmail.com>
2020-08-14 10:49:45 -0300

    Pequeños cambios de nombres y comentarios

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,18 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
+const server = require("express").Router();
+const { Product } = require("../db.js");
 
-server.get('/', (req, res, next) => {
+server.get("/", (req, res, next) => {
 	Product.findAll()
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
 });
+
+// ruta de obtener todos los productos :: server/api/products/
+
+// ------------------
+// path="/catalogue" ---> Catalogue.jsx ->>> fetch a server/api/productos __> [] --> Catalogue --> ProductCard
+//  -----------------
 
 module.exports = server;
