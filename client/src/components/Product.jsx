ea997df6c87091a2d26bf3d84e607e7a936f0712
Francis Ricle <francisricleruiz@gmail.com>
2020-08-16 13:31:08 -0300

    fixes agrega productos y los muestra

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useSate } from "react";
+import React, { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
@@ -13,21 +13,21 @@ export default function Product({id, filtrarProduct}) {
 			<Container>
 			<Row>
 			<Col style={{alignSelf:'center'}}>
-				<Card.Img src="https://i.ibb.co/jhJXXRv/Botella-vino-1.jpg" />
+				<Card.Img src={img} />
 			</Col>
 				<Col>
 				<Card.Body>
-					<Card.Title>{/*name*/}<h1>Vino</h1></Card.Title>
-					<Card.Subtitle className="mb-2 text-muted"><h3>{cellarId}Mendoza</h3></Card.Subtitle>
-					<Card.Text text-align="justify">{/*description*/}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Text>
+					<Card.Title>{name}</Card.Title>
+					<Card.Subtitle className="mb-2 text-muted"><h3>{cellarId}</h3></Card.Subtitle>
+					<Card.Text text-align="justify">{description}</Card.Text>
 				</Card.Body>
 				<Card.Body>
 					<ListGroup className="list-group-flush">
-						<ListGroupItem ><h3>{/*price*/}$ 300</h3></ListGroupItem>
+						<ListGroupItem ><h3>{price}</h3></ListGroupItem>
 						{/* size="lg" style={{fontSize:'2rem'}}  */}
 						<ListGroupItem>Productos disponibles: {stock}</ListGroupItem>
-						<ListGroupItem>Categoría {/*categoryId*/}</ListGroupItem>
-						<ListGroupItem>Cepa {/*strainId**/}</ListGroupItem>
+						<ListGroupItem>Categoría {categoryId}</ListGroupItem>
+						<ListGroupItem>Cepa {strainId}</ListGroupItem>
 					</ListGroup>
 				</Card.Body>
 				</Col>
