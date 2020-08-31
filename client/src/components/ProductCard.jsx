e6faf12bfc3ba81cccf0aec8fa3aa71221ffcee8
Olivert Borges <olivert.ricardo@gmail.com>
2020-08-31 03:49:46 -0300

    Estilos de catalogo v1

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,11 +3,12 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import { NavLink } from "react-router-dom";
+import '../styles/ProductCard.css'
 
 export default function ProductCard({ id, name, cellar, price, img }) {
 	return (
 		<NavLink to={`/product/${id}`}>
-			<Card style={{ width: "15rem", height: "25rem", margin: "5px" }}>
+			<Card style={{ width: "15rem", height: "25rem", margin: "5px" }} className="efecto">
 				<Card.Img variant="top" src={img} style={{ padding: "5px" }} />
 				<Card.Body>
 					<Card.Title>{name}</Card.Title>
