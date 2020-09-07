553f2f02cae91c0470cf784764879ad4ace0766e
aghmnl <agustin.minetto@gmail.com>
2020-09-07 01:19:34 -0300

    Ahora el review vale la pena!

--- client/src/components/MyPurchases.jsx
+++ client/src/components/MyPurchases.jsx
@@ -44,44 +44,6 @@ export default function Mypurchases() {
 							</Container>
 						</Card>
 					</NavLink>
-					// purchase.products.map(product => (
-					// 	<Card style={{ width: "50rem", margin: "auto" }}>
-					// 		<Row>
-					// 			<Col sm="2" style={{ alignSelf: "center" }}>
-					// 				<Card.Img style={{ transform: "scale(0.5)" }} src={product.img}></Card.Img>
-					// 			</Col>
-					// 			<Col sm="3" style={{ alignSelf: "center" }}>
-					// 				<NavLink to={`/product/${product.id}`}>
-					// 					<Card.Title>{product.name}</Card.Title>
-					// 				</NavLink>
-					// 			</Col>
-					// 			<Col sm="2" style={{ alignSelf: "center" }}>
-					// 				<Card.Subtitle>$ {product.purchased_product.priceProduct}</Card.Subtitle>
-					// 			</Col>
-					// 			<Col sm="2" style={{ alignSelf: "center" }}>
-					// 				<Card.Subtitle>Cantidad: {product.purchased_product.quantity}</Card.Subtitle>
-					// 			</Col>
-					// 			<Col sm="2" style={{ alignSelf: "center" }}>
-					// 				<Card.Subtitle>{purchase.date}</Card.Subtitle>
-					// 			</Col>
-					// 			{!!purchase.status && purchase.status.name === "entregada" && (<Col>
-					// 				<Dropdown>
-					// 					<Dropdown.Toggle variant="success" id="dropdown-basic"></Dropdown.Toggle>
-					// 					<Dropdown.Menu>
-					// 						<Dropdown.Item>
-					// 							<NavLink to={`/formReview/${product.id}`}>Review</NavLink>
-					// 						</Dropdown.Item>
-					// 					</Dropdown.Menu>
-					// 				</Dropdown>
-					// 			</Col>)}
-					// 		</Row>
-					// 		<Row>
-					// 			<Col>
-					// 				<Card.Subtitle>$ {product.total}</Card.Subtitle>
-					// 			</Col>
-					// 		</Row>
-					// 	</Card>)
-					// )
 				))
 			) : (
 				<div>No hay nada</div>
